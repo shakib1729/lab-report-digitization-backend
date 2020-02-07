0402101a87ce38cff5dec04c594a72ff467cbfca
shakib1729 <39847281+shakib1729@users.noreply.github.com>
2020-02-07 19:22:34 +0530

    adding Deployment related files

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from flask_sqlalchemy import SQLAlchemy
 import re
 import base64
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = '/app/.apt/usr/bin/tesseract'
 
 app = Flask(__name__)
 CORS(app)
@@ -17,6 +17,48 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 
 db = SQLAlchemy(app)
+
+
+# from selenium import webdriver
+# driver=webdriver.Chrome(executable_path='C:\Program Files (x86)\Google\Chrome\chromedriver')
+# driver
+
+# driver.get('https://www.meditec.com/resourcestools/medical-reference-links/normal-lab-values/')
+# driver.maximize_window()
+
+# def isfloat(value):
+#   try:
+#     float(value)
+#     return True
+#   except ValueError:
+#     return False
+
+# from bs4 import BeautifulSoup
+# import re
+# ans = []
+# lis = driver.find_elements_by_xpath('//div[@id = "ValuesWrapper"]/ul')
+# for i in lis:
+#     data = i
+#     data = BeautifulSoup(data.get_attribute('innerHTML'),'html.parser')
+#     l = data.find_all('li')
+#     for t in l:
+#         temp = []
+#         try:
+#             j = t.string.split()
+#             print(j)
+#             if(isfloat(j[1])):
+#                 temp.append(j[0].lower())
+#                 temp.append(float(re.findall(r"[-+]?\d*\.\d+|\d+",j[1])[0]))
+#                 temp.append(float(re.findall(r"[-+]?\d*\.\d+|\d+",j[3])[0]))
+#             else:
+#                 temp.append(j[0].lower()+" " + j[1].lower())
+#                 temp.append(float(re.findall(r"[-+]?\d*\.\d+|\d+",j[2])[0]))
+#                 temp.append(float(re.findall(r"[-+]?\d*\.\d+|\d+",j[4])[0]))
+#             ans.append(temp)
+#         except IndexError:
+#             continue
+# ans
+
 
 # Class that reprsents a Table
 class User(db.Model):
@@ -57,26 +99,36 @@ def get_digital(file_path):
 	return user_dict
 
 def save_db(user_dict):
-	if 'lymphocytes' and 'monocytes' in user_dict:
-		user = User(
-					rbc_count = float(user_dict['rbc count']),
-					lymphocytes = float(user_dict['lymphocytes']),
-					monocytes = float(user_dict['monocytes'])
-					)
-	elif 'monocytes' in user_dict:
-		user = User(
-					rbc_count = float(user_dict['rbc count']),
-					monocytes = float(user_dict['monocytes'])
-					)
-	elif 'lymphocytes' in user_dict:
-		user = User(
-					rbc_count = float(user_dict['rbc count']),
-					lymphocytes = float(user_dict['lymphocytes'])
-					)
-	else:
-		user = User(
-					rbc_count = float(user_dict['rbc count'])
-					)
+	user = User()
+	if 'lymphocytes' in user_dict:
+		user.lymphocytes = float(user_dict['lymphocytes'])
+
+	if 'monocytes' in user_dict:
+		user.monocytes = float(user_dict['monocytes'])
+
+	if 'rbc count' in user_dict:
+		user.rbc_count = float(user_dict['rbc count'])
+
+	# if 'lymphocytes' and 'monocytes' in user_dict:
+	# 	user = User(
+	# 				rbc_count = float(user_dict['rbc count']),
+	# 				lymphocytes = float(user_dict['lymphocytes']),
+	# 				monocytes = float(user_dict['monocytes'])
+	# 				)
+	# elif 'monocytes' in user_dict:
+	# 	user = User(
+	# 				rbc_count = float(user_dict['rbc count']),
+	# 				monocytes = float(user_dict['monocytes'])
+	# 				)
+	# elif 'lymphocytes' in user_dict:
+	# 	user = User(
+	# 				rbc_count = float(user_dict['rbc count']),
+	# 				lymphocytes = float(user_dict['lymphocytes'])
+	# 				)
+	# else:
+	# 	user = User(
+	# 				rbc_count = float(user_dict['rbc count'])
+	# 				)
 	db.session.add(user)
 	db.session.commit()
 
@@ -86,14 +138,16 @@ def save_graph(s2):
 	x=np.arange(0,5*(len(s2)+2),5)
 	y1=x-x+5.6
 	y2=x-x+4.2
-	plt.plot(x,y1,"b--",linewidth=1)
-	plt.plot(x,y2,"b--",linewidth=1)
 	s1=[ 5*(i+1) for i in range(len(s2))]
-	for i in range(len(s2)):
-	    if s2[i]<5.6 and s2[i]>4.2:
-	        plt.plot(s1[i],s2[i],marker='o', markersize=5,color="g")
-	    else:
-	        plt.plot(s1[i],s2[i],marker='o', markersize=5,color="r")
+	plt.rcParams["figure.figsize"] = [15, 8]
+	plt.plot(s1,s2,marker = 'o')
+	plt.plot(x,y1,"b--",linewidth=2)
+	plt.plot(x,y2,"b--",linewidth=2)
+	# for i in range(len(s2)):
+	#     if s2[i]<5.6 and s2[i]>4.2:
+	#         plt.plot(s1[i],s2[i],marker='o', markersize=5,color="g")
+	#     else:
+	#         plt.plot(s1[i],s2[i],marker='o', markersize=5,color="r")
 	plt.grid()
 	saved_file_path = 'testplot.png'
 	plt.savefig(saved_file_path)  # Save the graph
@@ -109,20 +163,23 @@ def getdigital():
 	if request.method == 'POST':
 		currFile = request.files['file']
 		file_name = secure_filename(currFile.filename)
-		file_path = os.path.join('./uploads', file_name)
+		# file_path = os.path.join('./uploads', file_name)##
 		# print(file_path)
-		currFile.save(file_path)
-		user_dict = get_digital(file_path)
-		# print(user_dict)
+		# currFile.save(file_path)###
+		currFile.save(file_name)
+		user_dict = get_digital(file_name)
+		# user_dict = get_digital(file_path)##
+		print(user_dict)
 		# save to DB
-		save_db(user_dict)
+		save_db(user_dict)###
 		# print(rbc_counts)
 		# user_dict['rbc_array'] = rbc_counts
 		
 		# user = [('user_data', user_dict), ('rbc_graph_url', rbc_graph_url)]
 		# user[rbc_graph_url] = rbc_graph_url
 		# print(user_dict)
-		return jsonify(user_dict),200
+		# return "doneeee"
+		return jsonify(user_dict),200##
 
 	return "Hello from /getdigital"
 
