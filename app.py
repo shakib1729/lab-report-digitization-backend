d8cb67d4e40bc98d9ad8d9926ab42a06dd89d17c
shakib1729 <39847281+shakib1729@users.noreply.github.com>
2020-07-02 22:21:18 +0530

    adding files

--- app.py
+++ app.py
@@ -3,62 +3,20 @@ import os
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
 import pytesseract
-from PIL import Image
 import cv2
+import matplotlib.pyplot as plt
+import numpy as np
 from flask_sqlalchemy import SQLAlchemy
 import re
 import base64
 
 pytesseract.pytesseract.tesseract_cmd = '/app/.apt/usr/bin/tesseract'
-
 app = Flask(__name__)
 CORS(app)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 
 db = SQLAlchemy(app)
-
-
-# from selenium import webdriver
-# driver=webdriver.Chrome(executable_path='C:\Program Files (x86)\Google\Chrome\chromedriver')
-# driver
-
-# driver.get('https://www.meditec.com/resourcestools/medical-reference-links/normal-lab-values/')
-# driver.maximize_window()
-
-# def isfloat(value):
-#   try:
-#     float(value)
-#     return True
-#   except ValueError:
-#     return False
-
-# from bs4 import BeautifulSoup
-# import re
-# ans = []
-# lis = driver.find_elements_by_xpath('//div[@id = "ValuesWrapper"]/ul')
-# for i in lis:
-#     data = i
-#     data = BeautifulSoup(data.get_attribute('innerHTML'),'html.parser')
-#     l = data.find_all('li')
-#     for t in l:
-#         temp = []
-#         try:
-#             j = t.string.split()
-#             print(j)
-#             if(isfloat(j[1])):
-#                 temp.append(j[0].lower())
-#                 temp.append(float(re.findall(r"[-+]?\d*\.\d+|\d+",j[1])[0]))
-#                 temp.append(float(re.findall(r"[-+]?\d*\.\d+|\d+",j[3])[0]))
-#             else:
-#                 temp.append(j[0].lower()+" " + j[1].lower())
-#                 temp.append(float(re.findall(r"[-+]?\d*\.\d+|\d+",j[2])[0]))
-#                 temp.append(float(re.findall(r"[-+]?\d*\.\d+|\d+",j[4])[0]))
-#             ans.append(temp)
-#         except IndexError:
-#             continue
-# ans
-
 
 # Class that reprsents a Table
 class User(db.Model):
@@ -80,14 +38,14 @@ def checkNumber(s):
     return s.replace('.','',1).isdigit()
 
 def get_digital(file_path):
-	img=cv2.imread(file_path)
+	img=cv2.imread(file_path, cv2.IMREAD_GRAYSCALE) # Open the image in Grayscale mode
 	text = pytesseract.image_to_string(img).lower()
 	text = re.sub('[_?!@#$|]', '', text)
 	textWords = text.split()
 	user_dict = {}
 	for i in range(len(textWords)):
 	    curr_term = ''
-	    for j in range(5): 
+	    for j in range(5):  # Check by also combining next 5 elements
 	        if i+j+1 >= len(textWords):
 	            break
 	        curr_term += textWords[i+j]
@@ -111,15 +69,13 @@ def save_db(user_dict):
 	db.session.add(user)
 	db.session.commit()
 
-import matplotlib.pyplot as plt
-import numpy as np
-def save_graph(s2):
-	x=np.arange(0,5*(len(s2)+2),5)
+def save_graph(arr):
+	x=np.arange(0,5*(len(arr)+2),5)
 	y1=x-x+5.6
 	y2=x-x+4.2
-	s1=[ 5*(i+1) for i in range(len(s2))]
+	arr2=[ 5*(i+1) for i in range(len(arr))]
 	plt.rcParams["figure.figsize"] = [15, 8]
-	plt.plot(s1,s2,marker = 'o')
+	plt.plot(arr2,arr,marker = 'o')
 	plt.plot(x,y1,"b--",linewidth=2)
 	plt.plot(x,y2,"b--",linewidth=2)
 	plt.grid()
@@ -135,15 +91,17 @@ def index():
 @app.route('/getdigital', methods=['GET', 'POST'])
 def getdigital():
 	if request.method == 'POST':
+		# Get the file from the POST request
 		currFile = request.files['file']
+		# Save the file to the specified path
 		file_name = secure_filename(currFile.filename)
-		currFile.save(file_name)
-		user_dict = get_digital(file_name)
-		print(user_dict)
+		file_path = os.path.join('./uploads', file_name)
+		currFile.save(file_path)	
 
+		user_dict = get_digital(file_path)
 		# save to DB
 		save_db(user_dict)
-		return jsonify(user_dict),200##
+		return jsonify(user_dict),200
 
 	return "Hello from /getdigital"
 
@@ -151,8 +109,8 @@ def getdigital():
 @app.route('/getrbc')
 def getgraph():
 	rbc_counts = [user.rbc_count for user in User.query.all()] # Create an array using the values in database
-	print(rbc_counts)
-	rbc_graph_url = save_graph(rbc_counts) # Save Graph using of the array created from the database
+	res = [i for i in rbc_counts if i] 
+	rbc_graph_url = save_graph(res) # Save Graph using the array created from the database
 	with open(rbc_graph_url, "rb") as imageFile:
 		strr = base64.b64encode(imageFile.read()) # Convert image to base64
 		strr = strr.decode('utf-8') # Convert that base 64 from 'bytes' type to 'str'
@@ -162,8 +120,7 @@ def getgraph():
 def getlymphocytes():
 	lymphocytes_counts = [user.lymphocytes for user in User.query.all()] # Create an array using the values in database
 	res = [i for i in lymphocytes_counts if i] 
-	print(res)
-	lymphocytes_counts_url = save_graph(res) # Save Graph using of the array created from the database
+	lymphocytes_counts_url = save_graph(res) # Save Graph using the array created from the database
 	with open(lymphocytes_counts_url, "rb") as imageFile:
 		strr = base64.b64encode(imageFile.read()) # Convert image to base64
 		strr = strr.decode('utf-8') # Convert that base 64 from 'bytes' type to 'str'
@@ -173,8 +130,7 @@ def getlymphocytes():
 def getmonocytes():
 	monocytes_counts = [user.monocytes for user in User.query.all()] # Create an array using the values in database
 	res = [i for i in monocytes_counts if i]
-	print(res)
-	monocytes_counts_url = save_graph(res) # Save Graph using of the array created from the database
+	monocytes_counts_url = save_graph(res) # Save Graph using the array created from the database
 	with open(monocytes_counts_url, "rb") as imageFile:
 		strr = base64.b64encode(imageFile.read()) # Convert image to base64
 		strr = strr.decode('utf-8') # Convert that base 64 from 'bytes' type to 'str'
